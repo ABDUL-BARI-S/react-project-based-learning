8c052410fd6917e108d0e9ff134dd51fcde00349
Abdul Bari <abdulbariss147@gmail.com>
2026-08-28 23:07:13 +0530

    Restructured the react-folder

--- Foodie_App/react.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-
-
-const heading = React.createElement('h1',{id : 'head'},'Hello REACT How are you');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<h1 id="heading">Hello React World hello</h1>)
