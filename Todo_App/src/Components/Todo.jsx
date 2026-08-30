47128c99e161c7aa60ceb2b19ab6366eaf84db9f
Abdul Bari <abdulbariss147@gmail.com>
2026-08-30 23:03:22 +0530

    Added edit functionality and updated README

--- Todo_App/src/Components/Todo.jsx
+++ Todo_App/src/Components/Todo.jsx
@@ -2,36 +2,52 @@ import React from 'react'
 import { useState } from 'react';
 
 function Todo() {
-  const [task, setTask] = useState("");
-    const [todos, setTodos] = useState([]);
+
+    const [task, setTask] = useState("");
+    const [todos, setTodos] = useState([]); // ARRAY
+
     const [editIndex, setEditIndex] = useState(null);
     const [editTask, setEditTask] = useState("");
     
 
 
-    const editTodo  = (index) => {
-      if(editIndex !== null){
-        <input type='text'></input>
-      }
-      const setEditTask = [...todos];
-      updatedTodos[index] = editTask;
-      setTodos(updatedTodos)
-      setEditIndex(null);
-  }
+   // ADDING A TODO
   const addTodo = () => {
     if (task.trim() === "") return;
-    setTodos([...todos, task]);
+    setTodos([...todos, task.trim()]);
     setTask("");
     }
-    const deleteTodo = (index) => {
-        setTodos(todos.filter((task, i) => i !== index));
+    // DELETING A TODO
+    const deleteTodo = (index) => {  // 0 !== 0
+        setTodos(todos.filter((task,i) => i !== index));
     };
+
+    const editTodo = (index) => {
+      setEditIndex(index);
+      setEditTask(todos[index]);
+    }
+    const saveTodo = () => {
+      if(editTask.trim() === "") return;
+      setTodos(todos.map((todo, index) => {
+
+        if(index === editIndex){
+          return editTask.trim();
+        }
+        return todo;
+        
+      }))
+      setEditIndex(null);
+      setEditTask("");
+    }
 
 
   return (
     <div className='container'>
       <h1>TO-DO List App</h1>
       <div className="child">
+
+        {editIndex === null ?
+       <>
         <input type="text"
           placeholder='add a todo'
           value={task}
@@ -39,18 +55,30 @@ function Todo() {
             setTask(e.target.value)
           }}
         />
-        <button onClick={addTodo}>Add</button>
+        <button onClick={addTodo} >Add</button>
+         </> :
+         <>
+         <input type='text'
+         value={editTask}
+
+         onChange={(e) => {
+          setEditTask(e.target.value);
+         }}
+         /> 
+          <button onClick={saveTodo} >Save</button>
+         </>
+         }
       </div>
+
+      {/* {DISPLAY PART} */}
       <ul>
         {todos.length === 0 ? (<p>No Tasks Exists </p>)
          : todos.map((todo,index) => {
           return (
               <li key={index}>{todo}
                   <div className="btns">
-                      <button onClick={editTodo}>Edit</button>
-                  <button onClick={() => {
-                      deleteTodo(index)
-              }}>Complete</button>
+                  <button onClick={() => editTodo(index)}>Edit</button>
+                  <button onClick={() => deleteTodo(index)}>Delete</button>
                   </div></li>
           )
         })}
